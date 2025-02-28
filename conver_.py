7e66092588a5c4d535d4f48ca324be95d936f18c
Andressa Karen <lqsandressa@gmail.com>
2025-02-28 16:12:43 -0300

    atualização

--- conver_.py
+++ conver_.py
@@ -3,10 +3,11 @@ import pytesseract
 import pyttsx3
 # C:\Program Files\Tesseract-OCR
 
-img = cv2.imread('test.jpeg')
+img = cv2.imread('4.jpeg')
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\Tesseract.exe'
 resultado = pytesseract.image_to_string(img)
-engine = pyttsx3.init()
+# engine = pyttsx3.init()
 print(resultado)
-engine.say(resultado)
-engine.runAndWait()
+# engine.say(resultado)
+# engine.runAndWait()
+
